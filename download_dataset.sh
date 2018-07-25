b8e96bd3da8d83152246e6838f9260c39a697a2d
104062337 <david80531@gmail.com>
2018-07-25 14:49:11 +0800

    download_dataset.sh

---
+++ download_dataset.sh
@@ -0,0 +1,17 @@
+cd ..
+
+echo $PWD"/action_dataset"
+
+if [ -d $PWD"/action_dataset" ]; then
+    # 目錄 /path/to/dir 存在
+    echo "action_dataset already exist."
+else
+    wget "http://crcv.ucf.edu/data/UCF101/UCF101.rar"
+
+    unrar x UCF101.rar
+
+    rm -rf UCF101.rar
+
+    mv ./UCF-101 ./action_dataset
+    echo "action_dataset install finished"
+fi
